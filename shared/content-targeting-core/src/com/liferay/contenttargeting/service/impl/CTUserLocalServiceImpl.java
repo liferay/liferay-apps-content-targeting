fc95ea97db45fef77676c2666e7a8ae46cc444f7
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2013-12-19 12:36:00 +0100

    WCM-9 Adds add, remove and update method to CTUser local service

--- shared/content-targeting-core/src/com/liferay/contenttargeting/service/impl/CTUserLocalServiceImpl.java
+++ shared/content-targeting-core/src/com/liferay/contenttargeting/service/impl/CTUserLocalServiceImpl.java
@@ -14,7 +14,16 @@
 
 package com.liferay.contenttargeting.service.impl;
 
+import com.liferay.contenttargeting.model.CTUser;
 import com.liferay.contenttargeting.service.base.CTUserLocalServiceBaseImpl;
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+
+import java.util.Date;
 
 /**
  * The implementation of the c t user local service.
@@ -31,4 +40,64 @@ import com.liferay.contenttargeting.service.base.CTUserLocalServiceBaseImpl;
  * @see com.liferay.contenttargeting.service.CTUserLocalServiceUtil
  */
 public class CTUserLocalServiceImpl extends CTUserLocalServiceBaseImpl {
+
+	@Override
+	public CTUser addUser(
+			long userId, String lastIp, String typeSettings,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		User user = UserLocalServiceUtil.getUser(userId);
+		long groupId = serviceContext.getScopeGroupId();
+
+		Date now = new Date();
+
+		long CTUserId = CounterLocalServiceUtil.increment();
+
+		CTUser CTUser = ctUserPersistence.create(CTUserId);
+
+		CTUser.setGroupId(groupId);
+		CTUser.setCompanyId(CTUser.getCompanyId());
+
+		if (user != null) {
+			CTUser.setUserId(user.getUserId());
+			CTUser.setUserName(user.getFullName());
+		}
+
+		CTUser.setCreateDate(serviceContext.getCreateDate(now));
+		CTUser.setModifiedDate(serviceContext.getModifiedDate(now));
+		CTUser.setLastIp(lastIp);
+		CTUser.setTypeSettings(typeSettings);
+
+		ctUserPersistence.update(CTUser);
+
+		return CTUser;
+	}
+
+	@Override
+	public CTUser deleteUser(long CTUserId)
+		throws PortalException, SystemException {
+
+		return ctUserPersistence.remove(CTUserId);
+	}
+
+	@Override
+	public CTUser updateUser(
+			long userId, String lastIp, String typeSettings,
+			ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		Date now = new Date();
+
+		CTUser CTUser = ctUserPersistence.findByPrimaryKey(userId);
+
+		CTUser.setModifiedDate(serviceContext.getModifiedDate(now));
+		CTUser.setLastIp(lastIp);
+		CTUser.setTypeSettings(typeSettings);
+
+		ctUserPersistence.update(CTUser);
+
+		return CTUser;
+	}
+
 }
