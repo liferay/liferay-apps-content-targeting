a0d1a0f21473ee1da20b6db535ee37d2497ad109
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-01-09 14:55:24 +0100

    WCM-10 Copy include method from parent (just copy-paste)

--- portlets/content-targeting-portlet/docroot/WEB-INF/src/com/liferay/contenttargeting/portlet/ContentTargetingPortlet.java
+++ portlets/content-targeting-portlet/docroot/WEB-INF/src/com/liferay/contenttargeting/portlet/ContentTargetingPortlet.java
@@ -16,12 +16,110 @@ package com.liferay.contenttargeting.portlet;
 
 import com.liferay.contenttargeting.api.model.RulesRegistry;
 import com.liferay.contenttargeting.portlet.internal.RulesRegistryFactory;
+import com.liferay.portal.kernel.io.unsync.UnsyncStringWriter;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.template.TemplateConstants;
+import com.liferay.portal.kernel.template.TemplateException;
+import com.liferay.portal.kernel.template.TemplateManagerUtil;
+import com.liferay.portal.kernel.template.TemplateResource;
+import com.liferay.portal.kernel.template.TemplateResourceLoaderUtil;
+import com.liferay.portal.kernel.template.TemplateTaglibSupportProvider;
+import com.liferay.portal.kernel.util.UnsyncPrintWriterPool;
 import com.liferay.util.bridges.freemarker.FreeMarkerPortlet;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.portlet.MimeResponse;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
 
 /**
  * @author Eduardo Garcia
  */
 public class ContentTargetingPortlet extends FreeMarkerPortlet {
+
+	@Override
+	protected void include(
+			String path, PortletRequest portletRequest,
+			PortletResponse portletResponse, String lifecycle)
+		throws IOException, PortletException {
+
+		PortletContext portletContext = getPortletContext();
+
+		String servletContextName = portletContext.getPortletContextName();
+
+		String resourcePath = servletContextName.concat(
+			TemplateConstants.SERVLET_SEPARATOR).concat(path);
+
+		boolean resourceExists = false;
+
+		try {
+			resourceExists = TemplateResourceLoaderUtil.hasTemplateResource(
+				TemplateConstants.LANG_TYPE_FTL, resourcePath);
+		}
+		catch (TemplateException te) {
+			throw new IOException(te.getMessage());
+		}
+
+		if (!resourceExists) {
+			_log.error(path + " is not a valid include");
+		}
+		else {
+			try {
+				TemplateResource templateResource =
+					TemplateResourceLoaderUtil.getTemplateResource(
+						TemplateConstants.LANG_TYPE_FTL, resourcePath);
+
+				Template template = TemplateManagerUtil.getTemplate(
+					TemplateConstants.LANG_TYPE_FTL, templateResource, false);
+
+				TemplateTaglibSupportProvider templateTaglibSupportProvider =
+					getTaglibSupportProvider();
+
+				if (templateTaglibSupportProvider != null) {
+					templateTaglibSupportProvider.addTaglibSupport(
+						template, servletContextName, portletRequest,
+						portletResponse);
+				}
+
+				template.put("portletContext", getPortletContext());
+				template.put(
+					"userInfo",
+					portletRequest.getAttribute(PortletRequest.USER_INFO));
+
+				Writer writer = null;
+
+				if (portletResponse instanceof MimeResponse) {
+					MimeResponse mimeResponse = (MimeResponse)portletResponse;
+
+					writer = UnsyncPrintWriterPool.borrow(
+						mimeResponse.getWriter());
+				}
+				else {
+					writer = new UnsyncStringWriter();
+				}
+
+				template.processTemplate(writer);
+			}
+			catch (Exception e) {
+				throw new PortletException(e);
+			}
+		}
+
+		if (clearRequestParameters) {
+			if (lifecycle.equals(PortletRequest.RENDER_PHASE)) {
+				portletResponse.setProperty("clear-request-parameters", "true");
+			}
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		ContentTargetingPortlet.class);
 
 	private RulesRegistry _rulesRegistry =
 		RulesRegistryFactory.getRulesRegistryFactory();
