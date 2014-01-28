c2ae866b6dd5979ba5b988ad78a2ef570c5be53e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-01-28 16:37:20 +0100

    WCM-7 Adds a new path util class

---
+++ portlets/content-targeting-portlet/docroot/WEB-INF/src/com/liferay/contenttargeting/portlet/TargetedContentDisplayPath.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.portlet;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class TargetedContentDisplayPath {
+
+	public static final String CONFIGURATION =
+		"html/targeted_content_display/configuration.ftl";
+
+	public static final String EDIT_RULE =
+		"html/targeted_content_display/edit_rule.ftl";
+
+	public static final String ERROR = "html/error.ftl";
+
+	public static final String VIEW = "html/targeted_content_display/view.ftl";
+
+}
