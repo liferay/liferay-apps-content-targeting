4765c6b868073bced0e27e52cda940d17da1f2a2
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2014-01-29 17:02:00 +0100

    WCM-14 Added test

---
+++ shared/content-targeting-core/test/unit/com/liferay/contenttargeting/service/impl/BaseOsgiTestPlugin.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.service.impl;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public class BaseOsgiTestPlugin {
+
+	@Deployment
+	public static WebArchive getDeployment() throws FileNotFoundException {
+		File buildFile = new File("build.xml");
+		Project p = new Project();
+		p.setUserProperty("ant.file", buildFile.getAbsolutePath());
+		p.init();
+		ProjectHelper helper = ProjectHelper.getProjectHelper();
+		p.addReference("ant.projectHelper", helper);
+		helper.parse(p, buildFile);
+		p.executeTarget("jar");
+
+		WebArchive wabFile = ShrinkWrap.createFromZipFile(
+			WebArchive.class,
+			new File(p.getProperty("plugin.file")));
+
+		wabFile.addClass(BaseOsgiTestPlugin.class);
+
+		WebArchive webArchive = ShrinkWrap.create(WebArchive.class, "test.war");
+		webArchive.merge(wabFile);
+
+		return webArchive;
+	}
+
+}
---
+++ shared/content-targeting-core/test/unit/com/liferay/contenttargeting/service/impl/UserSegmentLocalServiceImplTest.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.service.impl;
+
+import com.liferay.arquillian.container.enrichers.OSGi;
+import com.liferay.contenttargeting.model.UserSegment;
+import com.liferay.contenttargeting.service.UserSegmentLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalService;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.model.AssetCategory;
+import com.liferay.portlet.asset.service.AssetCategoryLocalService;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@RunWith(Arquillian.class)
+public class UserSegmentLocalServiceImplTest extends BaseOsgiTestPlugin {
+
+	@OSGi private UserSegmentLocalService userSegmentLocalService;
+	@OSGi private UserLocalService userLocalService;
+	@OSGi private AssetCategoryLocalService assetCategoryLocalService;
+	private User defaultUser;
+
+	@Before
+	public void prepare() throws SystemException, PortalException {
+		long defaultCompanyId = PortalUtil.getDefaultCompanyId();
+
+		defaultUser = userLocalService.getDefaultUser(defaultCompanyId);
+	}
+
+	@Test
+	public void testAddUserSegment() throws Exception {
+
+		int initUserSegmentsCount =
+			userSegmentLocalService.getUserSegmentsCount();
+
+		UserSegment userSegment = userSegmentLocalService.addUserSegment(
+			defaultUser.getUserId(),
+			new HashMap<Locale, String>() {{
+				put(LocaleUtil.getDefault(), "test-category");
+			}},
+			null, new ServiceContext());
+
+		Assert.assertEquals(
+			initUserSegmentsCount + 1,
+			userSegmentLocalService.getUserSegmentsCount());
+
+		AssetCategory assetCategory =
+			assetCategoryLocalService.fetchAssetCategory(
+				userSegment.getAssetCategoryId());
+
+		Assert.assertNotNull(assetCategory);
+
+		userSegmentLocalService.deleteUserSegment(
+			userSegment.getUserSegmentId());
+
+		Assert.assertEquals(
+			initUserSegmentsCount,
+			userSegmentLocalService.getUserSegmentsCount());
+	}
+
+}
