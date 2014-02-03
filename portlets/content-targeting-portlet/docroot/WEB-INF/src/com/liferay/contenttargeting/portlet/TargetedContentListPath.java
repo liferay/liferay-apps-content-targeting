2c75248f9db54727963d24e1f5283cc7195c47a2
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-02-03 08:44:30 +0100

    WCM-6 Adds new portlet targeted content list

---
+++ portlets/content-targeting-portlet/docroot/WEB-INF/src/com/liferay/contenttargeting/portlet/TargetedContentListPath.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.portlet;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class TargetedContentListPath {
+
+	public static final String ERROR = "html/error.ftl";
+
+	public static final String VIEW = "html/targeted_content_list/view.ftl";
+
+}
---
+++ portlets/content-targeting-portlet/docroot/WEB-INF/src/com/liferay/contenttargeting/portlet/TargetedContentListPortlet.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.portlet;
+
+import com.liferay.contenttargeting.model.UserSegment;
+import com.liferay.contenttargeting.portlet.util.QueryRule;
+import com.liferay.contenttargeting.portlet.util.QueryRuleUtil;
+import com.liferay.contenttargeting.portlet.util.UnavailableServiceException;
+import com.liferay.contenttargeting.service.UserSegmentLocalService;
+import com.liferay.contenttargeting.util.UserSegmentUtil;
+import com.liferay.contenttargeting.util.WebKeys;
+import com.liferay.osgi.util.OsgiServiceUnavailableException;
+import com.liferay.osgi.util.ServiceTrackerUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.template.Template;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.AssetRendererFactoryRegistryUtil;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.model.AssetRenderer;
+import com.liferay.portlet.asset.model.AssetRendererFactory;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.template.TemplateHashModel;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.UnavailableException;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class TargetedContentListPortlet extends CTFreeMarkerPortlet {
+
+	protected void populateContext(
+			String path, PortletRequest portletRequest,
+			PortletResponse portletResponse, Template template)
+		throws Exception {
+
+		BeansWrapper wrapper = BeansWrapper.getDefaultInstance();
+
+		TemplateHashModel staticModels = wrapper.getStaticModels();
+
+		populateViewContext(
+			path, portletRequest, portletResponse, template, staticModels);
+	}
+
+	protected void populateViewContext(
+			String path, PortletRequest portletRequest,
+			PortletResponse portletResponse, Template template,
+			TemplateHashModel staticModels)
+		throws Exception {
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(
+		TargetedContentListPortlet.class);
+
+}
