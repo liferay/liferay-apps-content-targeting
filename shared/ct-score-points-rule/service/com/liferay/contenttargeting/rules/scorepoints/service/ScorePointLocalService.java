ealonso/temp-wcm-plugins

WCM-3 Autogenerated

shared/ct-score-points-rule/service/com/liferay/contenttargeting/rules/scorepoints/service/ScorePointLocalService.java
@@ -247,4 +247,28 @@ public interface ScorePointLocalService extends BaseLocalService,
 	public java.lang.Object invokeMethod(java.lang.String name,
 		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
 		throws java.lang.Throwable;
+
+	/**
+	* NOTE FOR DEVELOPERS:
+	*
+	* Never reference this interface directly. Always use {@link com.liferay.contenttargeting.rules.scorepoints.service.ScorePointLocalServiceUtil} to access the score point local service.
+	*/
+	public com.liferay.contenttargeting.rules.scorepoints.model.ScorePoint addScorePoints(
+		long ctUserId, long assetCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public com.liferay.contenttargeting.rules.scorepoints.model.ScorePoint addScorePoints(
+		long ctUserId, long assetCategoryId, long points)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public long getPoints(long ctUserId, long assetCategoryId)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public long incrementPoints(long ctUserId, long assetCategoryId, long points)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public com.liferay.contenttargeting.rules.scorepoints.model.ScorePoint updateScorePoints(
+		long ctUserId, long assetCategoryId, long points)
+		throws com.liferay.portal.kernel.exception.SystemException;
 }

liferay/liferay-apps-content-targeting

shared/ct-score-points-rule/service/com/liferay/contenttargeting/rules/scorepoints/service/ScorePointLocalService.java
@@ -247,4 +247,24 @@ public interface ScorePointLocalService extends BaseLocalService,
 	public java.lang.Object invokeMethod(java.lang.String name,
 		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
 		throws java.lang.Throwable;
+
+	/**
+	* NOTE FOR DEVELOPERS:
+	*
+	* Never reference this interface directly. Always use {@link com.liferay.contenttargeting.rules.scorepoints.service.ScorePointLocalServiceUtil} to access the score point local service.
+	*/
+	public com.liferay.contenttargeting.rules.scorepoints.model.ScorePoint addScorePoints(
+		long ctUserId, long userSegmentId, long points)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public long getPoints(long ctUserId, long userSegmentId)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public long incrementPoints(long ctUserId, long userSegmentId, long points)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
+	public com.liferay.contenttargeting.rules.scorepoints.model.ScorePoint updateScorePoints(
+		long ctUserId, long userSegmentId, long points)
+		throws com.liferay.portal.kernel.exception.SystemException;
 }