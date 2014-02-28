25bf6e7f59b9c64ff88fa13f129f9c53ccc08200
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-02-28 10:43:02 +0000

    WCM-36 Adds a js module to make the search by ajax

---
+++ portlets/content-targeting-portlet/docroot/js/content_targeting/search.js
@@ -0,0 +1,133 @@
+AUI.add(
+	'liferay-ajax-search',
+	function(A) {
+		var Lang = A.Lang;
+
+		var SearchImpl = A.Component.create (
+			{
+				AUGMENTS: [A.AutoCompleteBase],
+
+				EXTENDS: A.Base,
+
+				NAME: 'searchimpl',
+
+				prototype: {
+					initializer: function() {
+						var instance = this;
+
+						this._bindUIACBase();
+						this._syncUIACBase();
+					}
+				}
+			}
+		);
+
+		var AjaxSearch = A.Component.create(
+			{
+				EXTENDS: SearchImpl,
+
+				NAME: 'ajaxsearch',
+
+				ATTRS: {
+					minQueryLength: {
+						validator: Lang.isNumber,
+						value: 0
+					},
+
+					queryDelay: {
+						validator: Lang.isNumber,
+						value: 300
+					},
+
+					resultFilters: {
+						setter: '_setResultFilters',
+						value: 'phraseMatch'
+					},
+
+					resultTextLocator: {
+						setter: '_setLocator',
+						value: 'search'
+					}
+				}
+			}
+		);
+
+		var AjaxContentSearch = A.Component.create(
+			{
+				AUGMENTS: [Liferay.PortletBase],
+
+				EXTENDS: A.Base,
+
+				NAME: 'ajaxcontentsearch',
+
+				ATTRS: {
+					contentPanel: {
+						setter: A.one
+					},
+					inputNode: {
+						setter: A.one
+					},
+					resourceURL: {
+						validator: Lang.isString
+					}
+				},
+
+				prototype: {
+					initializer: function(config) {
+						var instance = this;
+
+						var contentSearch = new AjaxSearch(
+							{
+								inputNode: instance.get('inputNode')
+							}
+						);
+
+						instance._contentPanel = instance.get('contentPanel');
+						instance._resourceURL = instance.get('resourceURL');
+
+						instance._search = contentSearch;
+
+						instance._bindUISearch();
+					},
+
+					_afterSuccess: function(event) {
+						var instance = this;
+
+						instance._contentPanel.setContent(event.currentTarget.get('responseData'));
+					},
+
+					_bindUISearch: function() {
+						var instance = this;
+
+						instance._search.after('query', instance._refreshContentList, instance);
+					},
+
+					_refreshContentList: function(event) {
+						var instance = this;
+
+						A.io.request(
+							instance._resourceURL,
+							{
+								after: {
+									success: A.bind('_afterSuccess', instance)
+								},
+								data: instance.ns(
+									{
+										keywords: instance.get('inputNode').val()
+									}
+								)
+							}
+						);
+					}
+				}
+			}
+		);
+
+		Liferay.AjaxContentSearch = AjaxContentSearch;
+		Liferay.AjaxSearch = AjaxSearch;
+	},
+	'',
+	{
+		requires: ['aui-base', 'aui-io-request', 'autocomplete-base', 'autocomplete-filters', 'liferay-portlet-base']
+	}
+);
