epgarcia/temp-wcm-plugins

WCM-55 Refactor

shared/content-targeting-core/service/com/liferay/contenttargeting/api/model/Rule.java
@@ -14,7 +14,7 @@
 
 package com.liferay.contenttargeting.api.model;
 
-import com.liferay.contenttargeting.model.CTUser;
+import com.liferay.anonymoususer.model.AnonymousUser;
 import com.liferay.contenttargeting.model.RuleInstance;
 
 import java.util.Locale;
@@ -39,11 +39,12 @@ public interface Rule {
 	 * Returns <code>true</code> if the user complies with the rule instance.
 	 *
 	 * @param  ruleInstance the rule instance with stored configuration
-	 * @param  ctUser the user who evaluates the rule
+	 * @param  anonymousUser the user who evaluates the rule
 	 * @return <code>true</code> if the user complies with the rule instance;
 	 *         <code>false</code> otherwise
 	 */
-	public boolean evaluate(RuleInstance ruleInstance, CTUser ctUser)
+	public boolean evaluate(
+			RuleInstance ruleInstance, AnonymousUser anonymousUser)
 		throws Exception;
 
 	/**

shared/content-targeting-core/src/com/liferay/contenttargeting/hook/action/JournalViewAction.java
@@ -14,8 +14,8 @@
 
 package com.liferay.contenttargeting.hook.action;
 
-import com.liferay.contenttargeting.model.CTUser;
-import com.liferay.contenttargeting.util.CTUserUtil;
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.anonymoususers.util.AnonymousUserUtil;
 import com.liferay.contenttargeting.util.WebKeys;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
@@ -54,13 +54,14 @@ public class JournalViewAction extends BaseStrutsPortletAction {
 			return forward;
 		}
 
-		CTUser ctUser = CTUserUtil.getCTUser(renderRequest, renderResponse);
+		AnonymousUser anonymousUser = AnonymousUserUtil.getAnonymousUser(
+			renderRequest, renderResponse);
 
 		Message message = new Message();
 
 		message.put("className", JournalArticle.class.getName());
 		message.put("classPK", getClassPK(article));
-		message.put("ctUserId", ctUser.getCTUserId());
+		message.put("anonymousUserId", anonymousUser.getAnonymousUserId());
 		message.put("groupId", themeDisplay.getScopeGroupId());
 
 		MessageBusUtil.sendMessage("liferay/analytics", message);

shared/content-targeting-core/src/com/liferay/contenttargeting/hook/action/LayoutViewAction.java
@@ -14,8 +14,8 @@
 
 package com.liferay.contenttargeting.hook.action;
 
-import com.liferay.contenttargeting.model.CTUser;
-import com.liferay.contenttargeting.util.CTUserUtil;
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.anonymoususers.util.AnonymousUserUtil;
 import com.liferay.contenttargeting.util.WebKeys;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
@@ -49,13 +49,14 @@ public class LayoutViewAction extends BaseStrutsAction {
 			return actionForward;
 		}
 
-		CTUser ctUser = CTUserUtil.getCTUser(request, response);
+		AnonymousUser anonymousUser = AnonymousUserUtil.getAnonymousUser(
+			request, response);
 
 		Message message = new Message();
 
 		message.put("className", Layout.class.getName());
 		message.put("classPK", layout.getPrimaryKey());
-		message.put("ctUserId", ctUser.getCTUserId());
+		message.put("anonymousUserId", anonymousUser.getAnonymousUserId());
 		message.put("groupId", layout.getGroupId());
 
 		MessageBusUtil.sendMessage("liferay/analytics", message);

shared/content-targeting-core/src/com/liferay/contenttargeting/hook/filter/UserSegmentFilter.java
@@ -14,11 +14,11 @@
 
 package com.liferay.contenttargeting.hook.filter;
 
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.anonymoususers.util.AnonymousUserUtil;
 import com.liferay.contenttargeting.api.model.RulesEngine;
-import com.liferay.contenttargeting.model.CTUser;
 import com.liferay.contenttargeting.model.UserSegment;
 import com.liferay.contenttargeting.service.UserSegmentLocalServiceUtil;
-import com.liferay.contenttargeting.util.CTUserUtil;
 import com.liferay.contenttargeting.util.ContentTargetingUtil;
 import com.liferay.contenttargeting.util.WebKeys;
 import com.liferay.osgi.util.ServiceTrackerUtil;
@@ -70,11 +70,13 @@ public class UserSegmentFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse)servletResponse;
 
 		try {
-			CTUser ctUser = CTUserUtil.getCTUser(request, response);
+			AnonymousUser anonymousUser = AnonymousUserUtil.getAnonymousUser(
+				request, response);
 
 			long[] groupIds = getGroupIds(request);
 
-			long[] userSegmentsIds = getMatchesUserSegmentIds(groupIds, ctUser);
+			long[] userSegmentsIds = getMatchesUserSegmentIds(
+				groupIds, anonymousUser);
 
 			if (ArrayUtil.isNotEmpty(userSegmentsIds)) {
 				request.setAttribute(WebKeys.USER_SEGMENT_IDS, userSegmentsIds);
@@ -87,7 +89,8 @@ public class UserSegmentFilter implements Filter {
 		filterChain.doFilter(servletRequest, servletResponse);
 	}
 
-	public long[] getMatchesUserSegmentIds(long[] groupIds, CTUser ctUser)
+	public long[] getMatchesUserSegmentIds(
+			long[] groupIds, AnonymousUser anonymousUser)
 		throws Exception {
 
 		if (ArrayUtil.isEmpty(groupIds)) {
@@ -100,7 +103,7 @@ public class UserSegmentFilter implements Filter {
 			UserSegmentLocalServiceUtil.getUserSegments(groupIds);
 
 		for (UserSegment userSegment : userSegments) {
-			if (matches(ctUser, userSegment)) {
+			if (matches(anonymousUser, userSegment)) {
 				userSegmentIds.add(userSegment.getUserSegmentId());
 			}
 		}
@@ -113,14 +116,15 @@ public class UserSegmentFilter implements Filter {
 		_intiRulesEngine();
 	}
 
-	public boolean matches(CTUser ctUser, UserSegment userSegment)
+	public boolean matches(AnonymousUser anonymousUser, UserSegment userSegment)
 		throws Exception {
 
 		if (_rulesEngine == null) {
 			_intiRulesEngine();
 		}
 
-		return _rulesEngine.matches(ctUser, userSegment.getRuleInstances());
+		return _rulesEngine.matches(
+			anonymousUser, userSegment.getRuleInstances());
 	}
 
 	protected long[] getGroupIds(HttpServletRequest request)

shared/ct-score-points-rule/src/com/liferay/contenttargeting/rules/scorepoints/ScorePointsRule.java
@@ -18,7 +18,7 @@ import aQute.bnd.annotation.component.Component;
 
 import com.liferay.contenttargeting.api.model.BaseRule;
 import com.liferay.contenttargeting.api.model.Rule;
-import com.liferay.contenttargeting.model.CTUser;
+import com.liferay.contenttargeting.model.AnonymousUser;
 import com.liferay.contenttargeting.model.RuleInstance;
 import com.liferay.contenttargeting.rules.scorepoints.service.ScorePointLocalServiceUtil;
 import com.liferay.portal.kernel.json.JSONException;
@@ -41,7 +41,8 @@ import javax.portlet.PortletResponse;
 public class ScorePointsRule extends BaseRule {
 
 	@Override
-	public boolean evaluate(RuleInstance ruleInstance, CTUser ctUser)
+	public boolean evaluate(
+			RuleInstance ruleInstance, AnonymousUser anonymousUser)
 		throws Exception {
 
 		String typeSettings = ruleInstance.getTypeSettings();
@@ -50,10 +51,11 @@ public class ScorePointsRule extends BaseRule {
 
 		long scorePoints = jsonObj.getLong("scorePoints");
 
-		long ctUserScorePoints = ScorePointLocalServiceUtil.getPoints(
-			ctUser.getCTUserId(), ruleInstance.getUserSegmentId());
+		long anonymousUserScorePoints = ScorePointLocalServiceUtil.getPoints(
+			anonymousUser.getAnonymousUserId(),
+			ruleInstance.getUserSegmentId());
 
-		if (ctUserScorePoints >= scorePoints) {
+		if (anonymousUserScorePoints >= scorePoints) {
 			return true;
 		}
 

liferay/liferay-apps-content-targeting

shared/content-targeting-core/service/com/liferay/contenttargeting/api/model/Rule.java
@@ -14,7 +14,7 @@
 
 package com.liferay.contenttargeting.api.model;
 
-import com.liferay.contenttargeting.model.CTUser;
+import com.liferay.anonymoususers.model.AnonymousUser;
 import com.liferay.contenttargeting.model.RuleInstance;
 
 import java.util.Locale;
@@ -39,11 +39,12 @@ public interface Rule {
 	 * Returns <code>true</code> if the user complies with the rule instance.
 	 *
 	 * @param  ruleInstance the rule instance with stored configuration
-	 * @param  ctUser the user who evaluates the rule
+	 * @param  anonymousUser the user who evaluates the rule
 	 * @return <code>true</code> if the user complies with the rule instance;
 	 *         <code>false</code> otherwise
 	 */
-	public boolean evaluate(RuleInstance ruleInstance, CTUser ctUser)
+	public boolean evaluate(
+			RuleInstance ruleInstance, AnonymousUser anonymousUser)
 		throws Exception;
 
 	/**

shared/content-targeting-core/src/com/liferay/contenttargeting/hook/action/JournalViewAction.java
@@ -14,8 +14,8 @@
 
 package com.liferay.contenttargeting.hook.action;
 
-import com.liferay.contenttargeting.model.CTUser;
-import com.liferay.contenttargeting.util.CTUserUtil;
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.anonymoususers.util.AnonymousUsersUtil;
 import com.liferay.contenttargeting.util.WebKeys;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
@@ -54,13 +54,14 @@ public class JournalViewAction extends BaseStrutsPortletAction {
 			return forward;
 		}
 
-		CTUser ctUser = CTUserUtil.getCTUser(renderRequest, renderResponse);
+		AnonymousUser anonymousUser = AnonymousUsersUtil.getAnonymousUser(
+			renderRequest, renderResponse);
 
 		Message message = new Message();
 
 		message.put("className", JournalArticle.class.getName());
 		message.put("classPK", getClassPK(article));
-		message.put("ctUserId", ctUser.getCTUserId());
+		message.put("anonymousUserId", anonymousUser.getAnonymousUserId());
 		message.put("groupId", themeDisplay.getScopeGroupId());
 
 		MessageBusUtil.sendMessage("liferay/analytics", message);

shared/content-targeting-core/src/com/liferay/contenttargeting/hook/action/LayoutViewAction.java
@@ -14,8 +14,8 @@
 
 package com.liferay.contenttargeting.hook.action;
 
-import com.liferay.contenttargeting.model.CTUser;
-import com.liferay.contenttargeting.util.CTUserUtil;
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.anonymoususers.util.AnonymousUsersUtil;
 import com.liferay.contenttargeting.util.WebKeys;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageBusUtil;
@@ -49,13 +49,14 @@ public class LayoutViewAction extends BaseStrutsAction {
 			return actionForward;
 		}
 
-		CTUser ctUser = CTUserUtil.getCTUser(request, response);
+		AnonymousUser anonymousUser = AnonymousUsersUtil.getAnonymousUser(
+			request, response);
 
 		Message message = new Message();
 
 		message.put("className", Layout.class.getName());
 		message.put("classPK", layout.getPrimaryKey());
-		message.put("ctUserId", ctUser.getCTUserId());
+		message.put("anonymousUserId", anonymousUser.getAnonymousUserId());
 		message.put("groupId", layout.getGroupId());
 
 		MessageBusUtil.sendMessage("liferay/analytics", message);

shared/content-targeting-core/src/com/liferay/contenttargeting/hook/filter/UserSegmentFilter.java
@@ -14,11 +14,11 @@
 
 package com.liferay.contenttargeting.hook.filter;
 
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.anonymoususers.util.AnonymousUsersUtil;
 import com.liferay.contenttargeting.api.model.RulesEngine;
-import com.liferay.contenttargeting.model.CTUser;
 import com.liferay.contenttargeting.model.UserSegment;
 import com.liferay.contenttargeting.service.UserSegmentLocalServiceUtil;
-import com.liferay.contenttargeting.util.CTUserUtil;
 import com.liferay.contenttargeting.util.ContentTargetingUtil;
 import com.liferay.contenttargeting.util.WebKeys;
 import com.liferay.osgi.util.ServiceTrackerUtil;
@@ -70,11 +70,13 @@ public class UserSegmentFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse)servletResponse;
 
 		try {
-			CTUser ctUser = CTUserUtil.getCTUser(request, response);
+			AnonymousUser anonymousUser = AnonymousUsersUtil.getAnonymousUser(
+				request, response);
 
 			long[] groupIds = getGroupIds(request);
 
-			long[] userSegmentsIds = getMatchesUserSegmentIds(groupIds, ctUser);
+			long[] userSegmentsIds = getMatchesUserSegmentIds(
+				groupIds, anonymousUser);
 
 			if (ArrayUtil.isNotEmpty(userSegmentsIds)) {
 				request.setAttribute(WebKeys.USER_SEGMENT_IDS, userSegmentsIds);
@@ -87,7 +89,8 @@ public class UserSegmentFilter implements Filter {
 		filterChain.doFilter(servletRequest, servletResponse);
 	}
 
-	public long[] getMatchesUserSegmentIds(long[] groupIds, CTUser ctUser)
+	public long[] getMatchesUserSegmentIds(
+			long[] groupIds, AnonymousUser anonymousUser)
 		throws Exception {
 
 		if (ArrayUtil.isEmpty(groupIds)) {
@@ -100,7 +103,7 @@ public class UserSegmentFilter implements Filter {
 			UserSegmentLocalServiceUtil.getUserSegments(groupIds);
 
 		for (UserSegment userSegment : userSegments) {
-			if (matches(ctUser, userSegment)) {
+			if (matches(anonymousUser, userSegment)) {
 				userSegmentIds.add(userSegment.getUserSegmentId());
 			}
 		}
@@ -113,14 +116,15 @@ public class UserSegmentFilter implements Filter {
 		_intiRulesEngine();
 	}
 
-	public boolean matches(CTUser ctUser, UserSegment userSegment)
+	public boolean matches(AnonymousUser anonymousUser, UserSegment userSegment)
 		throws Exception {
 
 		if (_rulesEngine == null) {
 			_intiRulesEngine();
 		}
 
-		return _rulesEngine.matches(ctUser, userSegment.getRuleInstances());
+		return _rulesEngine.matches(
+			anonymousUser, userSegment.getRuleInstances());
 	}
 
 	protected long[] getGroupIds(HttpServletRequest request)

shared/ct-score-points-rule/src/com/liferay/contenttargeting/rules/scorepoints/ScorePointsRule.java
@@ -16,9 +16,9 @@ package com.liferay.contenttargeting.rules.scorepoints;
 
 import aQute.bnd.annotation.component.Component;
 
+import com.liferay.anonymoususers.model.AnonymousUser;
 import com.liferay.contenttargeting.api.model.BaseRule;
 import com.liferay.contenttargeting.api.model.Rule;
-import com.liferay.contenttargeting.model.CTUser;
 import com.liferay.contenttargeting.model.RuleInstance;
 import com.liferay.contenttargeting.rules.scorepoints.service.ScorePointLocalServiceUtil;
 import com.liferay.portal.kernel.json.JSONException;
@@ -41,7 +41,8 @@ import javax.portlet.PortletResponse;
 public class ScorePointsRule extends BaseRule {
 
 	@Override
-	public boolean evaluate(RuleInstance ruleInstance, CTUser ctUser)
+	public boolean evaluate(
+			RuleInstance ruleInstance, AnonymousUser anonymousUser)
 		throws Exception {
 
 		String typeSettings = ruleInstance.getTypeSettings();
@@ -50,10 +51,11 @@ public class ScorePointsRule extends BaseRule {
 
 		long scorePoints = jsonObj.getLong("scorePoints");
 
-		long ctUserScorePoints = ScorePointLocalServiceUtil.getPoints(
-			ctUser.getCTUserId(), ruleInstance.getUserSegmentId());
+		long anonymousUserScorePoints = ScorePointLocalServiceUtil.getPoints(
+			anonymousUser.getAnonymousUserId(),
+			ruleInstance.getUserSegmentId());
 
-		if (ctUserScorePoints >= scorePoints) {
+		if (anonymousUserScorePoints >= scorePoints) {
 			return true;
 		}
 