0eb61f63f09a33df38a690c094feb946a8292239
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-05-19 17:15:33 +0200

    WCM-62 Add Campaign Content Report (skeleton)

---
+++ shared/ct-campaign-content-report/src/com/liferay/contenttargeting/reports/campaigncontent/CampaignContentReport.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.reports.campaigncontent;
+
+import aQute.bnd.annotation.component.Component;
+
+import com.liferay.contenttargeting.api.model.BaseReport;
+import com.liferay.contenttargeting.api.model.Report;
+import com.liferay.contenttargeting.model.Campaign;
+
+/**
+ * @author Eduardo Garcia
+ */
+@Component(immediate = true, provide = Report.class)
+public class CampaignContentReport extends BaseReport {
+
+	@Override
+	public String getIcon() {
+		return "icon-eye-open";
+	}
+
+	@Override
+	public String getReportType() {
+		return Campaign.class.getName();
+	}
+
+}
---
+++ shared/ct-campaign-content-report/src/templates/ct_report.ftl
@@ -0,0 +1,4 @@
+<#assign aui = PortletJspTagLibs["/META-INF/aui.tld"] />
+<#assign liferay_ui = PortletJspTagLibs["/META-INF/liferay-ui.tld"] />
+
+<@liferay_ui["message"] key="${campaign.getName(locale)}" />
