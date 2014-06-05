9feb85ea3cbc448a9c40159a0968050b1a3bfadb
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-06-05 18:12:12 +0200

    WCM-77 Fix renamed modules

--- shared/report-campaign-content/service/com/liferay/contenttargeting/reports/campaigncontent/service/ClpSerializer.java
+++ shared/report-campaign-content/service/com/liferay/contenttargeting/reports/campaigncontent/service/ClpSerializer.java
@@ -59,7 +59,7 @@ public class ClpSerializer {
 						new Class<?>[] { String.class });
 
 				String portletPropsServletContextName = (String)getMethod.invoke(null,
-						"ct-campaign-content-report-deployment-context");
+						"report-campaign-content-deployment-context");
 
 				if (Validator.isNotNull(portletPropsServletContextName)) {
 					_servletContextName = portletPropsServletContextName;
@@ -75,7 +75,7 @@ public class ClpSerializer {
 			if (Validator.isNull(_servletContextName)) {
 				try {
 					String propsUtilServletContextName = PropsUtil.get(
-							"ct-campaign-content-report-deployment-context");
+							"report-campaign-content-deployment-context");
 
 					if (Validator.isNotNull(propsUtilServletContextName)) {
 						_servletContextName = propsUtilServletContextName;
@@ -90,7 +90,7 @@ public class ClpSerializer {
 			}
 
 			if (Validator.isNull(_servletContextName)) {
-				_servletContextName = "ct-campaign-content-report";
+				_servletContextName = "report-campaign-content";
 			}
 
 			return _servletContextName;
--- shared/report-user-segment-content/service/com/liferay/contenttargeting/reports/usersegmentcontent/service/ClpSerializer.java
+++ shared/report-user-segment-content/service/com/liferay/contenttargeting/reports/usersegmentcontent/service/ClpSerializer.java
@@ -59,7 +59,7 @@ public class ClpSerializer {
 						new Class<?>[] { String.class });
 
 				String portletPropsServletContextName = (String)getMethod.invoke(null,
-						"ct-user-segment-content-report-deployment-context");
+						"report-user-segment-content-deployment-context");
 
 				if (Validator.isNotNull(portletPropsServletContextName)) {
 					_servletContextName = portletPropsServletContextName;
@@ -75,7 +75,7 @@ public class ClpSerializer {
 			if (Validator.isNull(_servletContextName)) {
 				try {
 					String propsUtilServletContextName = PropsUtil.get(
-							"ct-user-segment-content-report-deployment-context");
+							"report-user-segment-content-deployment-context");
 
 					if (Validator.isNotNull(propsUtilServletContextName)) {
 						_servletContextName = propsUtilServletContextName;
@@ -90,7 +90,7 @@ public class ClpSerializer {
 			}
 
 			if (Validator.isNull(_servletContextName)) {
-				_servletContextName = "ct-user-segment-content-report";
+				_servletContextName = "report-user-segment-content";
 			}
 
 			return _servletContextName;
