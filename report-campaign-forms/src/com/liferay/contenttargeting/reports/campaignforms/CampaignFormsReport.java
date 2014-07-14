e518af66cb617d751a3e075fe674d351207027aa
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-07-14 13:00:08 +0200

    WCM-74 Add report for campaign forms (Skeleton)

---
+++ report-campaign-forms/src/com/liferay/contenttargeting/reports/campaignforms/CampaignFormsReport.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.reports.campaignforms;
+
+import com.liferay.contenttargeting.api.model.BaseReport;
+import com.liferay.contenttargeting.api.model.Report;
+import com.liferay.contenttargeting.model.Campaign;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Eduardo Garcia
+ */
+@Component(immediate = true, service = Report.class)
+public class CampaignFormsReport extends BaseReport {
+
+	@Override
+	public String getIcon() {
+		return "icon-list-alt";
+	}
+
+	@Override
+	public String getReportType() {
+		return Campaign.class.getName();
+	}
+
+	@Override
+	public void updateReport() {
+	}
+
+	@Override
+	protected void populateContext(Map<String, Object> context) {
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(CampaignFormsReport.class);
+
+}
