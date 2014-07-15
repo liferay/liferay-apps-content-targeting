919eeb23372827d4d8d847aa2d2e2ae13c2aca5a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-07-15 16:54:46 +0200

    WCM-88 Implement regular role rule

--- rule-role/src/com/liferay/contenttargeting/rules/role/RegularRoleRule.java
+++ rule-role/src/com/liferay/contenttargeting/rules/role/RegularRoleRule.java
@@ -18,6 +18,7 @@ import com.liferay.anonymoususers.model.AnonymousUser;
 import com.liferay.contenttargeting.api.model.BaseRule;
 import com.liferay.contenttargeting.api.model.Rule;
 import com.liferay.contenttargeting.model.RuleInstance;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -28,28 +29,39 @@ import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.text.Format;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 import org.osgi.service.component.annotations.Component;
 
 /**
  * @author Julio Camarero
  */
 @Component(immediate = true, service = Rule.class)
-public class RoleRule extends BaseRule {
+public class RegularRoleRule extends BaseRule {
 
 	@Override
 	public boolean evaluate(
 			RuleInstance ruleInstance, AnonymousUser anonymousUser)
 		throws Exception {
 
-		return false;
+		long roleId = GetterUtil.getLong(ruleInstance.getTypeSettings());
+
+		return RoleLocalServiceUtil.hasUserRole(
+			anonymousUser.getUserId(), roleId);
 	}
 
 	@Override
@@ -59,7 +71,15 @@ public class RoleRule extends BaseRule {
 
 	@Override
 	public String getSummary(RuleInstance ruleInstance, Locale locale) {
-		String typeSettings = ruleInstance.getTypeSettings();
+		long roleId = GetterUtil.getLong(ruleInstance.getTypeSettings());
+
+		try {
+			Role role = RoleLocalServiceUtil.fetchRole(roleId);
+
+			return role.getTitle(locale);
+		}
+		catch (SystemException e) {
+		}
 
 		return StringPool.BLANK;
 	}
@@ -69,29 +89,33 @@ public class RoleRule extends BaseRule {
 		PortletRequest request, PortletResponse response, String id,
 		Map<String, String> values) {
 
-		JSONObject jsonObj = JSONFactoryUtil.createJSONObject();
-
-
-		return jsonObj.toString();
+		return values.get("roleId");
 	}
 
 	@Override
 	protected void populateContext(
 		RuleInstance ruleInstance, Map<String, Object> context) {
 
+		long roleId = 0;
+
 		if (ruleInstance != null) {
-			String typeSettings = ruleInstance.getTypeSettings();
+			roleId = GetterUtil.getLong(ruleInstance.getTypeSettings());
+		}
 
-			try {
-				JSONObject jsonObj = JSONFactoryUtil.createJSONObject(
-					typeSettings);
-			}
-			catch (JSONException jse) {
-			}
+		context.put("roleId", roleId);
+
+		Company company = (Company)context.get("company");
+
+		List<Role> roles = new ArrayList<Role>();
+
+		try {
+			roles = RoleLocalServiceUtil.getRoles(
+				company.getCompanyId(), new int[] {RoleConstants.TYPE_REGULAR});
 		}
-		else {
+		catch (SystemException e) {
 		}
+
+		context.put("roles", roles);
 	}
-
 
 }
--- rule-role/src/templates/ct_fields.ftl
+++ rule-role/src/templates/ct_fields.ftl
@@ -1,3 +1,8 @@
 <#assign aui = PortletJspTagLibs["/META-INF/aui.tld"] />
 <#assign liferay_ui = PortletJspTagLibs["/META-INF/liferay-ui.tld"] />
 
+<@aui["select"] label="" name="roleId">
+	<#list roles as role>
+		<@aui["option"] label="${role.getTitle(locale)}" selected=(role.getRoleId() == roleId) value=role.getRoleId() />
+	</#list>
+</@>
