6bcb956229000bd33952c29d2c07a7c2252aa631
Julio Camarero <julio.camarero@liferay.com>
2014-07-18 12:41:30 +0200

    WCM-59 Registry for Categories

---
+++ content-targeting-api/src/com/liferay/contenttargeting/api/model/BaseRuleCategory.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.api.model;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.security.permission.ResourceActionsUtil;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Julio Camarero
+ */
+public abstract class BaseRuleCategory implements RuleCategory {
+
+	@Activate
+	public void activate() {
+		if (_log.isDebugEnabled()) {
+			_log.debug("Rule Category activate: " + getClass().getSimpleName());
+		}
+	}
+
+	@Deactivate
+	public void deActivate() {
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				"Rule Category deactivate: " + getClass().getSimpleName());
+		}
+	}
+
+	@Override
+	public String getCategoryKey() {
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		String key = getClass().getName().concat(".description");
+
+		String description = ResourceActionsUtil.getModelResource(locale, key);
+
+		if (description.equals(key)) {
+			description = StringPool.BLANK;
+		}
+
+		return description;
+	}
+
+	@Override
+	public String getIcon() {
+		return "icon-archive";
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return ResourceActionsUtil.getModelResource(
+			locale, getClass().getName());
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(BaseRuleCategory.class);
+
+}
---
+++ content-targeting-api/src/com/liferay/contenttargeting/internal/DefaultRuleCategoriesRegistryImpl.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.internal;
+
+import com.liferay.contenttargeting.api.model.RuleCategoriesRegistry;
+import com.liferay.contenttargeting.api.model.RuleCategory;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+
+/**
+ * @author Julio Camarero
+ */
+@Component
+public class DefaultRuleCategoriesRegistryImpl
+	implements RuleCategoriesRegistry {
+
+	@Override
+	public Map<String, RuleCategory> getRuleCategories() {
+		return _ruleCategories;
+	}
+
+	@Override
+	public RuleCategory getRuleCategory(String ruleCategoryKey) {
+		return _ruleCategories.get(ruleCategoryKey);
+	}
+
+	@Reference(
+		unbind = "unregisterRuleCategory",
+		cardinality = ReferenceCardinality.MULTIPLE)
+	public void registerRuleCategory(RuleCategory ruleCategory) {
+		_ruleCategories.put(ruleCategory.getCategoryKey(), ruleCategory);
+	}
+
+	public void unregisterRuleCategory(RuleCategory ruleCategory) {
+		_ruleCategories.remove(ruleCategory);
+	}
+
+	private Map<String, RuleCategory> _ruleCategories =
+		new ConcurrentHashMap<String, RuleCategory>();
+
+}
