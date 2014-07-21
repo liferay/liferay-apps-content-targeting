f889a4c949c8cda941b5d3af3c0f658b81164918
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-07-21 18:02:01 +0200

    WCM-88 Test adds new test for usergroup rule

---
+++ rule-usergroup-member-test/src/com/liferay/usergroup/test/util/TestUtil.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.usergroup.test.util;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.Group;
+import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.Role;
+import com.liferay.portal.model.RoleConstants;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+
+import java.util.List;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class TestUtil {
+
+	public static Group addGroup() throws PortalException, SystemException {
+		return addGroup(
+			getCompanyId(), getUserId(), GroupConstants.DEFAULT_PARENT_GROUP_ID,
+			StringUtil.randomString(), StringUtil.randomString());
+	}
+
+	public static Group addGroup(
+			long companyId, long userId, long parentGroupId, String name,
+			String description)
+		throws PortalException, SystemException {
+
+		Group group = GroupLocalServiceUtil.fetchGroup(companyId, name);
+
+		if (group != null) {
+			return group;
+		}
+
+		int type = GroupConstants.TYPE_SITE_OPEN;
+		String friendlyURL =
+			StringPool.SLASH + FriendlyURLNormalizerUtil.normalize(name);
+		boolean site = true;
+		boolean active = true;
+		boolean manualMembership = true;
+		int membershipRestriction =
+			GroupConstants.DEFAULT_MEMBERSHIP_RESTRICTION;
+
+		return GroupLocalServiceUtil.addGroup(
+			userId, parentGroupId, null, 0,
+			GroupConstants.DEFAULT_LIVE_GROUP_ID, name, description, type,
+			manualMembership, membershipRestriction, friendlyURL, site, active,
+			getServiceContext());
+	}
+
+	public static long getCompanyId() {
+		return PortalUtil.getDefaultCompanyId();
+	}
+
+	public static ServiceContext getServiceContext()
+		throws PortalException, SystemException {
+
+		Group defaultGroup = GroupLocalServiceUtil.getGroup(
+			getCompanyId(), GroupConstants.GUEST);
+
+		return getServiceContext(defaultGroup.getGroupId(), getUserId());
+	}
+
+	public static ServiceContext getServiceContext(long groupId, long userId)
+		throws PortalException, SystemException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setCompanyId(PortalUtil.getDefaultCompanyId());
+		serviceContext.setScopeGroupId(groupId);
+		serviceContext.setUserId(userId);
+
+		return serviceContext;
+	}
+
+	public static User getUser() throws PortalException, SystemException {
+		if (_user == null) {
+			Role role = RoleLocalServiceUtil.getRole(
+				getCompanyId(), RoleConstants.ADMINISTRATOR);
+
+			List<User> users = UserLocalServiceUtil.getRoleUsers(
+					role.getRoleId(), 0, 2);
+
+			if (!users.isEmpty()) {
+				_user = users.get(0);
+
+				_userId = _user.getUserId();
+			}
+		}
+
+		return _user;
+	}
+
+	public static long getUserId() throws PortalException, SystemException {
+		if (_userId == 0) {
+			User user = getUser();
+
+			if (user != null) {
+				_userId = user.getUserId();
+			}
+		}
+
+		return _userId;
+	}
+
+	private static User _user;
+	private static long _userId;
+
+}
---
+++ rule-usergroup-member-test/test/integration/com/liferay/contenttargeting/rules/usergroupmember/UserGroupMemberTest.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.rules.usergroupmember;
+
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.anonymoususers.service.AnonymousUserLocalService;
+import com.liferay.contenttargeting.api.model.Rule;
+import com.liferay.contenttargeting.api.model.RulesRegistry;
+import com.liferay.contenttargeting.model.RuleInstance;
+import com.liferay.contenttargeting.service.RuleInstanceLocalService;
+import com.liferay.osgi.util.service.ServiceTrackerUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.UserGroup;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserGroupLocalServiceUtil;
+import com.liferay.usergroup.test.util.TestUtil;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@RunWith(Arquillian.class)
+public class UserGroupMemberTest {
+
+	@Before
+	public void setUp() {
+		try {
+			_bundle.start();
+		}
+		catch (BundleException e) {
+			e.printStackTrace();
+		}
+
+		_anonymousUserLocalService = ServiceTrackerUtil.getService(
+			AnonymousUserLocalService.class, _bundle.getBundleContext());
+		_ruleInstanceLocalService = ServiceTrackerUtil.getService(
+			RuleInstanceLocalService.class, _bundle.getBundleContext());
+		_rulesRegistry = ServiceTrackerUtil.getService(
+			RulesRegistry.class, _bundle.getBundleContext());
+	}
+
+	@Test
+	public void testUserGroupMemberRule() throws Exception {
+		ServiceContext serviceContext = TestUtil.getServiceContext();
+
+		AnonymousUser anonymousUser =
+			_anonymousUserLocalService.addAnonymousUser(
+				TestUtil.getUserId(), "127.0.0.1", StringPool.BLANK,
+				serviceContext);
+
+		Rule rule = _rulesRegistry.getRule("UserGroupMemberRule");
+
+		UserGroup userGroup =
+			UserGroupLocalServiceUtil.addUserGroup(
+				TestUtil.getUserId(), TestUtil.getCompanyId(),
+				StringUtil.randomString(), StringUtil.randomString(),
+				new ServiceContext());
+
+		UserGroupLocalServiceUtil.addUserUserGroup(
+			TestUtil.getUserId(), userGroup.getUserGroupId());
+
+		RuleInstance ruleInstance = _ruleInstanceLocalService.addRuleInstance(
+			TestUtil.getUserId(), rule.getRuleKey(), 0,
+			String.valueOf(userGroup.getUserGroupId()), serviceContext);
+
+		Assert.assertTrue(rule.evaluate(null, ruleInstance, anonymousUser));
+	}
+
+	private AnonymousUserLocalService _anonymousUserLocalService;
+
+	@ArquillianResource
+	private Bundle _bundle;
+
+	private RuleInstanceLocalService _ruleInstanceLocalService;
+	private RulesRegistry _rulesRegistry;
+
+}
