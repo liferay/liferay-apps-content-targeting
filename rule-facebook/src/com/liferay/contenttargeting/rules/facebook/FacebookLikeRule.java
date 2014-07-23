d8f74b5a0ccc077bb34270d6fdc11cc9462664d8
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-07-23 13:04:49 +0200

    WCM-98 Adds new like rule

---
+++ rule-facebook/src/com/liferay/contenttargeting/rules/facebook/FacebookLikeRule.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.rules.facebook;
+
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.contenttargeting.api.model.BaseRule;
+import com.liferay.contenttargeting.api.model.Rule;
+import com.liferay.contenttargeting.model.RuleInstance;
+import com.liferay.contenttargeting.rulecategories.SocialRuleCategory;
+import com.liferay.contenttargeting.rules.facebook.util.FacebookUtil;
+import com.liferay.portal.kernel.util.StringPool;
+
+import com.restfb.FacebookClient;
+import com.restfb.types.NamedFacebookType;
+import com.restfb.types.Page;
+import com.restfb.types.Post;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = Rule.class)
+public class FacebookLikeRule extends BaseRule {
+
+	@Activate
+	@Override
+	public void activate() {
+		super.activate();
+	}
+
+	@Deactivate
+	@Override
+	public void deActivate() {
+		super.deActivate();
+	}
+
+	@Override
+	public boolean evaluate(
+			HttpServletRequest request, RuleInstance ruleInstance,
+			AnonymousUser anonymousUser)
+		throws Exception {
+
+		FacebookClient facebookClient = new DefaultFacebookClient(
+			FacebookUtil.getAccessToken());
+
+		Page page = null;
+
+		try {
+			page = facebookClient.fetchObject(
+				ruleInstance.getTypeSettings(), Page.class);
+		}
+		catch (Exception e) {
+			return false;
+		}
+
+		Post.Likes postLikes = facebookClient.fetchObject(
+			"me/likes/" + page.getId(), Post.Likes.class);
+
+		List<NamedFacebookType> likes = postLikes.getData();
+
+		if (likes.size() > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public String getIcon() {
+		return "icon-facebook-sign";
+	}
+
+	@Override
+	public String getRuleCategoryKey() {
+		return SocialRuleCategory.KEY;
+	}
+
+	@Override
+	public String getSummary(RuleInstance ruleInstance, Locale locale) {
+		return ruleInstance.getTypeSettings();
+	}
+
+	@Override
+	public String processRule(
+		PortletRequest request, PortletResponse response, String id,
+		Map<String, String> values) {
+
+		return values.get("facebookName");
+	}
+
+	protected String getFormTemplatePath() {
+		return _FORM_TEMPLATE_PATH_LIKE;
+	}
+
+	@Override
+	protected void populateContext(
+		RuleInstance ruleInstance, Map<String, Object> context) {
+
+		String name = StringPool.BLANK;
+
+		if (ruleInstance != null) {
+			name = ruleInstance.getTypeSettings();
+		}
+
+		context.put("facebookName", name);
+	}
+
+	protected static final String _FORM_TEMPLATE_PATH_LIKE =
+		"templates/ct_fields_like.ftl";
+
+}
---
+++ rule-facebook/src/templates/ct_fields_like.ftl
@@ -0,0 +1,4 @@
+<#assign aui = PortletJspTagLibs["/META-INF/aui.tld"] />
+<#assign portlet = PortletJspTagLibs["/META-INF/liferay-portlet.tld"] />
+
+<@aui["input"] label="" name="facebookName" type="text" prefix="https://www.facebook.com/" value=facebookName />
