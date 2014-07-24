bc3e782ace92126411b22d19dd16a5f23a800cd0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-07-24 09:23:10 +0200

    WCM-98 Adds new rule for count the number of friends

---
+++ rule-facebook/src/com/liferay/contenttargeting/rules/facebook/FacebookFriendsRule.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.contenttargeting.rules.facebook;
+
+import com.liferay.anonymoususers.model.AnonymousUser;
+import com.liferay.contenttargeting.api.model.BaseRule;
+import com.liferay.contenttargeting.api.model.Rule;
+import com.liferay.contenttargeting.model.RuleInstance;
+import com.liferay.contenttargeting.rulecategories.SocialRuleCategory;
+import com.liferay.contenttargeting.rules.facebook.util.FacebookUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.json.JsonObject;
+import com.restfb.types.User;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(immediate = true, service = Rule.class)
+public class FacebookFriendsRule extends BaseRule {
+
+	@Activate
+	@Override
+	public void activate() {
+		super.activate();
+	}
+
+	@Deactivate
+	@Override
+	public void deActivate() {
+		super.deActivate();
+	}
+
+	@Override
+	public boolean evaluate(
+			HttpServletRequest request, RuleInstance ruleInstance,
+			AnonymousUser anonymousUser)
+		throws Exception {
+
+		FacebookClient facebookClient = new DefaultFacebookClient(
+			FacebookUtil.getAccessToken());
+
+		int numberOfFriends = GetterUtil.getInteger(
+			ruleInstance.getTypeSettings());
+
+		User user = facebookClient.fetchObject("me", User.class);
+
+		String query =
+			"SELECT friend_count FROM user WHERE uid = " + user.getId();
+
+		List<JsonObject> jsonObjects = facebookClient.executeFqlQuery(
+			query, JsonObject.class);
+
+		JsonObject jsonObject = jsonObjects.get(0);
+
+		long friendCount = GetterUtil.getLong(jsonObject.get("friend_count"));
+
+		if (friendCount >= numberOfFriends) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public String getIcon() {
+		return "icon-group";
+	}
+
+	@Override
+	public String getRuleCategoryKey() {
+		return SocialRuleCategory.KEY;
+	}
+
+	@Override
+	public String getSummary(RuleInstance ruleInstance, Locale locale) {
+		return ruleInstance.getTypeSettings();
+	}
+
+	@Override
+	public String processRule(
+		PortletRequest request, PortletResponse response, String id,
+		Map<String, String> values) {
+
+		return values.get("numberOfFriends");
+	}
+
+	protected String getFormTemplatePath() {
+		return _FORM_TEMPLATE_PATH_FRIENDS;
+	}
+
+	@Override
+	protected void populateContext(
+		RuleInstance ruleInstance, Map<String, Object> context) {
+
+		int numberOfFriends = 0;
+
+		if (ruleInstance != null) {
+			numberOfFriends = GetterUtil.getInteger(
+				ruleInstance.getTypeSettings());
+		}
+
+		context.put("numberOfFriends", numberOfFriends);
+	}
+
+	protected static final String _FORM_TEMPLATE_PATH_FRIENDS =
+		"templates/ct_fields_friends.ftl";
+
+}
---
+++ rule-facebook/src/templates/ct_fields_friends.ftl
@@ -0,0 +1,4 @@
+<#assign aui = PortletJspTagLibs["/META-INF/aui.tld"] />
+<#assign portlet = PortletJspTagLibs["/META-INF/liferay-portlet.tld"] />
+
+<@aui["input"] label="user-that-have-more-than" name="numberOfFriends" type="text" suffix="friends" value=numberOfFriends />
