79c4a2ad863ffd4807d23eb49c17e60c96db6845
Julio Camarero <julio.camarero@liferay.com>
2014-08-11 11:00:30 +0200

    WCM-66 remove index manually

--- report-campaign-tracking-action/sql/indexes.sql
+++ report-campaign-tracking-action/sql/indexes.sql
@@ -1,4 +1,3 @@
 create index IX_5720E6C9 on ContentTargeting_CampaignTAReport_CampaignTrackingAction (campaignId);
 create index IX_268FED54 on ContentTargeting_CampaignTAReport_CampaignTrackingAction (campaignId, modifiedDate);
-create index IX_C65E6152 on ContentTargeting_CampaignTAReport_CampaignTrackingAction (campaignId, userSegmentId, plid, elementId, eventType);
 create index IX_C4B66357 on ContentTargeting_CampaignTAReport_CampaignTrackingAction (campaignId, userSegmentId, referrerClassName, referrerClassPK, elementId, eventType);
