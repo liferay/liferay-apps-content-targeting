412aecf3ff0b16a05615c95f2ba60cd884d2d76e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-09-26 13:03:34 +0200

    WCM-213 indexes

--- analytics-api/sql/indexes.sql
+++ analytics-api/sql/indexes.sql
@@ -1,10 +1,8 @@
 create index IX_9015B32B on ContentTargeting_Analytics_AnalyticsEvent (anonymousUserId, className, classPK, eventType);
 create index IX_3F70B29A on ContentTargeting_Analytics_AnalyticsEvent (className, classPK, eventType);
 create index IX_EB75EA58 on ContentTargeting_Analytics_AnalyticsEvent (className, classPK, eventType, createDate);
-create index IX_1C801DC8 on ContentTargeting_Analytics_AnalyticsEvent (className, classPK, referrerClassName, referrerClassPK, eventType, createDate);
 create index IX_466C824E on ContentTargeting_Analytics_AnalyticsEvent (companyId);
 create index IX_F853CC0C on ContentTargeting_Analytics_AnalyticsEvent (companyId, createDate);
 create index IX_39D81875 on ContentTargeting_Analytics_AnalyticsEvent (elementId, eventType, createDate);
-create index IX_CDB73705 on ContentTargeting_Analytics_AnalyticsEvent (referrerClassName, referrerClassPK, elementId, eventType, createDate);
 
 create index IX_7176901E on ContentTargeting_Analytics_AnalyticsReferrer (analyticsEventId, referrerClassName, referrerClassPK);
