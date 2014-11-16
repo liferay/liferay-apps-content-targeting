79d33d5fc8cf11fc5b7a3102e505c23c8f90ea04
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-11-16 22:25:24 +0100

    WCM-34 Declare portlet as stageable

--- content-targeting-api/src/com/liferay/content/targeting/lar/ContentTargetingPortletDataHandler.java
+++ content-targeting-api/src/com/liferay/content/targeting/lar/ContentTargetingPortletDataHandler.java
@@ -18,7 +18,6 @@ import com.liferay.content.targeting.service.CampaignLocalService;
 import com.liferay.content.targeting.service.UserSegmentLocalService;
 import com.liferay.osgi.util.service.ServiceTrackerUtil;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
-import com.liferay.portal.kernel.lar.DataLevel;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 
 import javax.portlet.PortletPreferences;
@@ -32,13 +31,6 @@ import org.osgi.framework.FrameworkUtil;
  * @see    com.liferay.portal.kernel.lar.PortletDataHandler
  */
 public class ContentTargetingPortletDataHandler extends BasePortletDataHandler {
-
-	public ContentTargetingPortletDataHandler() {
-
-		// Prevent displaying the Content Targeting as a stageable application
-
-		setDataLevel(DataLevel.PORTLET_INSTANCE);
-	}
 
 	@Override
 	protected PortletPreferences doDeleteData(
