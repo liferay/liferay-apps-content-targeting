212f663987e7506c3ac00460817eae5e7347ce90
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-11-16 22:26:50 +0100

    WCM-34 Implement portlet export/import methods

--- content-targeting-api/src/com/liferay/content/targeting/lar/ContentTargetingPortletDataHandler.java
+++ content-targeting-api/src/com/liferay/content/targeting/lar/ContentTargetingPortletDataHandler.java
@@ -18,15 +18,23 @@ import com.liferay.content.targeting.model.Campaign;
 import com.liferay.content.targeting.model.UserSegment;
 import com.liferay.content.targeting.service.CampaignLocalService;
 import com.liferay.content.targeting.service.UserSegmentLocalService;
+import com.liferay.content.targeting.service.permission.ContentTargetingPermission;
+import com.liferay.content.targeting.service.persistence.CampaignExportActionableDynamicQuery;
+import com.liferay.content.targeting.service.persistence.UserSegmentExportActionableDynamicQuery;
 import com.liferay.osgi.util.service.ServiceTrackerUtil;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
+import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.portal.kernel.lar.StagedModelType;
+import com.liferay.portal.kernel.xml.Element;
+
+import java.util.List;
 
 import javax.portlet.PortletPreferences;
 import javax.portlet.UnavailableException;
 
-import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
-import com.liferay.portal.kernel.lar.StagedModelType;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
@@ -73,6 +81,93 @@ public class ContentTargetingPortletDataHandler extends BasePortletDataHandler {
 
 		return portletPreferences;
 	}
+
+	@Override
+	protected String doExportData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		portletDataContext.addPortletPermissions(RESOURCE_NAME);
+
+		Element rootElement = addExportDataRootElement(portletDataContext);
+
+		if (portletDataContext.getBooleanParameter(NAMESPACE, "campaigns")) {
+			ActionableDynamicQuery campaignActionableDynamicQuery =
+				new CampaignExportActionableDynamicQuery(portletDataContext);
+
+			campaignActionableDynamicQuery.performActions();
+		}
+
+		if (portletDataContext.getBooleanParameter(
+				NAMESPACE, "user-segments")) {
+
+			ActionableDynamicQuery userSegmentActionableDynamicQuery =
+				new UserSegmentExportActionableDynamicQuery(portletDataContext);
+
+			userSegmentActionableDynamicQuery.performActions();
+		}
+
+		return getExportDataRootElementString(rootElement);
+	}
+
+	@Override
+	protected PortletPreferences doImportData(
+			PortletDataContext portletDataContext, String portletId,
+			PortletPreferences portletPreferences, String data)
+		throws Exception {
+
+		portletDataContext.importPortletPermissions(
+			ContentTargetingPermission.RESOURCE_NAME);
+
+		if (portletDataContext.getBooleanParameter(
+				NAMESPACE, "user-segments")) {
+
+			Element userSegmentsElement =
+				portletDataContext.getImportDataGroupElement(UserSegment.class);
+
+			List<Element> userSegmentElements = userSegmentsElement.elements();
+
+			for (Element userSegmentElement : userSegmentElements) {
+				StagedModelDataHandlerUtil.importStagedModel(
+					portletDataContext, userSegmentElement);
+			}
+		}
+
+		if (portletDataContext.getBooleanParameter(NAMESPACE, "campaigns")) {
+			Element campaignsElement =
+				portletDataContext.getImportDataGroupElement(Campaign.class);
+
+			List<Element> campaignElements = campaignsElement.elements();
+
+			for (Element campaignElement : campaignElements) {
+				StagedModelDataHandlerUtil.importStagedModel(
+					portletDataContext, campaignElement);
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	protected void doPrepareManifestSummary(
+			PortletDataContext portletDataContext,
+			PortletPreferences portletPreferences)
+		throws Exception {
+
+		ActionableDynamicQuery campaignActionableDynamicQuery =
+			new CampaignExportActionableDynamicQuery(portletDataContext);
+
+		campaignActionableDynamicQuery.performCount();
+
+		ActionableDynamicQuery userSegmentActionableDynamicQuery =
+			new UserSegmentExportActionableDynamicQuery(portletDataContext);
+
+		userSegmentActionableDynamicQuery.performCount();
+	}
+
+	protected static final String RESOURCE_NAME =
+		"com.liferay.content.targeting";
 
 	private void _initServices() throws UnavailableException {
 		Bundle bundle = FrameworkUtil.getBundle(getClass());
