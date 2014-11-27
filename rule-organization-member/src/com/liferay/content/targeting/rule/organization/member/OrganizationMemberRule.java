ca2a34c0bc952bd529318d05ff5d4944fae499da
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-11-27 16:58:44 +0100

    WCM-34 Implement custom data export/import in Organization Member Rule

--- rule-organization-member/src/com/liferay/content/targeting/rule/organization/member/OrganizationMemberRule.java
+++ rule-organization-member/src/com/liferay/content/targeting/rule/organization/member/OrganizationMemberRule.java
@@ -18,12 +18,17 @@ import com.liferay.content.targeting.anonymous.users.model.AnonymousUser;
 import com.liferay.content.targeting.api.model.BaseRule;
 import com.liferay.content.targeting.api.model.Rule;
 import com.liferay.content.targeting.model.RuleInstance;
+import com.liferay.content.targeting.model.UserSegment;
 import com.liferay.content.targeting.rule.categories.UserAttributesRuleCategory;
 import com.liferay.content.targeting.util.ContentTargetingContextUtil;
 import com.liferay.content.targeting.util.PortletKeys;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.PortletDataException;
+import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.OrganizationConstants;
@@ -76,6 +81,35 @@ public class OrganizationMemberRule extends BaseRule {
 	}
 
 	@Override
+	public void exportData(
+			PortletDataContext portletDataContext, Element userSegmentElement,
+			UserSegment userSegment, Element ruleInstanceElement,
+			RuleInstance ruleInstance)
+		throws Exception {
+
+		long organizationId = GetterUtil.getLong(
+			ruleInstance.getTypeSettings());
+
+		Organization organization =
+			OrganizationLocalServiceUtil.fetchOrganization(organizationId);
+
+		if (organization != null ) {
+			ruleInstance.setTypeSettings(organization.getUuid());
+
+			portletDataContext.addReferenceElement(
+				userSegment, userSegmentElement, organization,
+				PortletDataContext.REFERENCE_TYPE_DEPENDENCY, true);
+
+			return;
+		}
+
+		throw new PortletDataException(
+			getExportImportErrorMessage(
+				userSegment, ruleInstance, Organization.class.getName(),
+				String.valueOf(organizationId), Constants.EXPORT));
+	}
+
+	@Override
 	public String getIcon() {
 		return "icon-globe";
 	}
@@ -104,6 +138,31 @@ public class OrganizationMemberRule extends BaseRule {
 		}
 
 		return StringPool.BLANK;
+	}
+
+	@Override
+	public void importData(
+			PortletDataContext portletDataContext, UserSegment userSegment,
+			RuleInstance ruleInstance)
+		throws Exception {
+
+		String organizationUuid = ruleInstance.getTypeSettings();
+
+		Organization organization =
+			OrganizationLocalServiceUtil.fetchOrganizationByUuidAndCompanyId(
+				organizationUuid, portletDataContext.getCompanyId());
+
+		if (organization != null ) {
+			ruleInstance.setTypeSettings(
+				String.valueOf(organization.getOrganizationId()));
+
+			return;
+		}
+
+		throw new PortletDataException(
+			getExportImportErrorMessage(
+				userSegment, ruleInstance, Organization.class.getName(),
+				organizationUuid, Constants.IMPORT));
 	}
 
 	@Override
