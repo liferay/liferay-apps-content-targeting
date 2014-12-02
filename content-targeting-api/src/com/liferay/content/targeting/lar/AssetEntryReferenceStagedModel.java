3ca302bb5844faffcf2329d91a80f87b3a6e7a87
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-12-02 15:01:24 +0100

    WCM-34 Add logic to handle validation of AssetEntry dependencies

---
+++ content-targeting-api/src/com/liferay/content/targeting/lar/AssetEntryReferenceStagedModel.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.lar;
+
+import com.liferay.portal.kernel.lar.StagedModelType;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.model.StagedModel;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.expando.model.ExpandoBridge;
+
+import java.io.Serializable;
+
+import java.util.Date;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class AssetEntryReferenceStagedModel implements StagedModel {
+
+	public AssetEntryReferenceStagedModel(AssetEntry assetEntry) {
+		_assetEntry = assetEntry;
+	}
+
+	@Override
+	public Object clone() {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getClassName() {
+		return _assetEntry.getClassName();
+	}
+
+	@Override
+	public long getCompanyId() {
+		return _assetEntry.getCompanyId();
+	}
+
+	@Override
+	public Date getCreateDate() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ExpandoBridge getExpandoBridge() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getModelClassName() {
+		return AssetEntryReferenceStagedModel.class.getName();
+	}
+
+	@Override
+	public Date getModifiedDate() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Serializable getPrimaryKeyObj() {
+		return _assetEntry.getClassPK();
+	}
+
+	@Override
+	public StagedModelType getStagedModelType() {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getTitle() {
+		return _assetEntry.getTitle(LocaleUtil.getDefault());
+	}
+
+	@Override
+	public String getUuid() {
+		return _assetEntry.getClassUuid();
+	}
+
+	@Override
+	public void setCompanyId(long companyId) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setCreateDate(Date date) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setModifiedDate(Date date) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void setUuid(String uuid) {
+		throw new UnsupportedOperationException();
+	}
+
+	private AssetEntry _assetEntry;
+
+}
---
+++ content-targeting-api/src/com/liferay/content/targeting/lar/AssetEntryReferenceStagedModelDataHandler.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.lar;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.BaseStagedModelDataHandler;
+import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.security.permission.ResourceActionsUtil;
+import com.liferay.portlet.asset.model.AssetEntry;
+import com.liferay.portlet.asset.service.AssetEntryLocalServiceUtil;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class AssetEntryReferenceStagedModelDataHandler
+	extends BaseStagedModelDataHandler<AssetEntryReferenceStagedModel> {
+
+	public static final String[] CLASS_NAMES = {
+		AssetEntryReferenceStagedModel.class.getName()};
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException, SystemException {
+	}
+
+	@Override
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	public String getDisplayName(
+		AssetEntryReferenceStagedModel assetEntryReferenceClassedModel) {
+
+		String title = assetEntryReferenceClassedModel.getTitle();
+
+		String type = ResourceActionsUtil.getModelResource(
+			LocaleUtil.getDefault(),
+			assetEntryReferenceClassedModel.getClassName());
+
+		return title + " - " + type;
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext,
+			AssetEntryReferenceStagedModel stagedModel)
+		throws Exception {
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext,
+			AssetEntryReferenceStagedModel stagedModel)
+		throws Exception {
+	}
+
+	@Override
+	protected boolean validateMissingReference(
+			String uuid, long companyId, long groupId)
+		throws Exception {
+
+		AssetEntry assetEntry = AssetEntryLocalServiceUtil.fetchEntry(
+			groupId, uuid);
+
+		if (assetEntry == null) {
+			return false;
+		}
+
+		return true;
+	}
+
+}
