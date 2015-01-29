917cc6a1a470e056d5135e86dfcb6e5aac079275
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-01-29 16:26:07 +0100

    WCM-280 Implement IP Range Rule

--- rule-ip-range/src/com/liferay/content/targeting/rule/ip.range/IpRangeRule.java
+++ rule-ip-range/src/com/liferay/content/targeting/rule/ip.range/IpRangeRule.java
@@ -14,13 +14,21 @@
 
 package com.liferay.content.targeting.rule.ip.range;
 
+import com.liferay.content.targeting.InvalidRuleException;
 import com.liferay.content.targeting.anonymous.users.model.AnonymousUser;
 import com.liferay.content.targeting.api.model.BaseRule;
 import com.liferay.content.targeting.api.model.Rule;
 import com.liferay.content.targeting.model.RuleInstance;
-import com.liferay.content.targeting.rule.categories.SampleRuleCategory;
+import com.liferay.content.targeting.rule.categories.SessionAttributesRuleCategory;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import java.util.Locale;
 import java.util.Map;
@@ -58,36 +66,127 @@ public class IpRangeRule extends BaseRule {
 			AnonymousUser anonymousUser)
 		throws Exception {
 
+		JSONObject jsonObj = JSONFactoryUtil.createJSONObject(
+			ruleInstance.getTypeSettings());
+
+		long userIp = _ipToLong(anonymousUser.getLastIp());
+		long ipFrom = _ipToLong(jsonObj.getString("ipFrom"));
+		long ipTo = _ipToLong(jsonObj.getString("ipTo"));
+
+		if ((userIp >= ipFrom) && (userIp <= ipTo)) {
+			return true;
+		}
+
 		return false;
 	}
 
 	@Override
 	public String getIcon() {
-		return "icon-puzzle";
+		return "icon-sitemap";
 	}
 
 	@Override
 	public String getRuleCategoryKey() {
-		return SampleRuleCategory.KEY;
+		return SessionAttributesRuleCategory.KEY;
 	}
 
 	@Override
 	public String getSummary(RuleInstance ruleInstance, Locale locale) {
-		return LanguageUtil.get(locale, ruleInstance.getTypeSettings());
+		String summary = StringPool.BLANK;
+
+		try {
+			JSONObject jsonObj = JSONFactoryUtil.createJSONObject(
+				ruleInstance.getTypeSettings());
+
+			String ipFrom = jsonObj.getString("ipFrom");
+			String ipTo = jsonObj.getString("ipTo");
+
+			if (Validator.isNotNull(ipFrom) && Validator.isNotNull(ipTo)) {
+				summary = LanguageUtil.format(
+					locale, "user-ip-is-between-x-and-x",
+					new Object[] {ipFrom, ipTo});
+			}
+		}
+		catch (Exception e) {
+		}
+
+		return summary;
 	}
 
 	@Override
 	public String processRule(
-		PortletRequest request, PortletResponse response, String id,
-		Map<String, String> values) {
+			PortletRequest request, PortletResponse response, String id,
+			Map<String, String> values)
+		throws InvalidRuleException {
 
-		return StringPool.BLANK;
+		String ipFrom = values.get("ipFrom");
+		String ipTo = values.get("ipTo");
+
+		try {
+			long lIpFrom = _ipToLong(ipFrom);
+			long lIpTo = _ipToLong(ipTo);
+
+			if (lIpFrom > lIpTo) {
+				throw new InvalidRuleException("invalid-ip-range");
+			}
+		}
+		catch (UnknownHostException uhe) {
+			throw new InvalidRuleException("invalid-ip-format");
+		}
+
+		JSONObject jsonObj = JSONFactoryUtil.createJSONObject();
+
+		jsonObj.put("ipFrom", ipFrom);
+		jsonObj.put("ipTo", ipTo);
+
+		return jsonObj.toString();
 	}
 
 	@Override
 	protected void populateContext(
 		RuleInstance ruleInstance, Map<String, Object> context,
 		Map<String, String> values) {
+
+		String ipFrom = StringPool.BLANK;
+		String ipTo = StringPool.BLANK;
+
+		if (!values.isEmpty()) {
+			ipFrom = values.get("ipFrom");
+			ipTo = values.get("ipTo");
+		}
+		else if (ruleInstance != null) {
+			String typeSettings = ruleInstance.getTypeSettings();
+
+			try {
+				JSONObject jsonObj = JSONFactoryUtil.createJSONObject(
+					typeSettings);
+
+				ipFrom = jsonObj.getString("ipFrom");
+				ipTo = jsonObj.getString("ipTo");
+			}
+			catch (JSONException jse) {
+			}
+		}
+
+		context.put("ipFrom", ipFrom);
+		context.put("ipTo", ipTo);
+	}
+
+	private long _ipToLong(String ip) throws UnknownHostException {
+		InetAddress inetAddress = InetAddress.getByName(ip);
+
+		inetAddress.getAddress();
+
+		byte[] octets = inetAddress.getAddress();
+
+		long result = 0;
+
+		for (byte octet : octets) {
+			result <<= 8;
+			result |= octet & 0xff;
+		}
+
+		return result;
 	}
 
 }
--- rule-ip-range/src/templates/ct_fields.ftl
+++ rule-ip-range/src/templates/ct_fields.ftl
@@ -3,4 +3,32 @@
 
 <#setting number_format="computer">
 
-This is the <b>Ip Range</b> rule.
+<@aui["input"] inlineField=true label="from" name="ipFrom" placeholder="192.0.0.1" value=ipFrom>
+	<@aui["validator"] errorMessage="invalid-ip-format" name="custom">
+		function(val, fieldNode, ruleValue) {
+			return validateIpAddress(val);
+		}
+	</@>
+</@>
+
+<@aui["input"] inlineField=true label="to" name="ipTo" placeholder="192.0.0.255" value=ipTo>
+	<@aui["validator"] errorMessage="invalid-ip-format" name="custom">
+		function(val, fieldNode, ruleValue) {
+			return validateIpAddress(val);
+		}
+	</@>
+</@>
+
+<@aui["script"]>
+	function validateIpAddress(ipAddress) {
+		var IPV4 = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/;
+		var IPV6 = /^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$/;
+
+		if (ipAddress.match(IPV4) || ipAddress.match(IPV6)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+</@>
