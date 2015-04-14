9c5572306bd6ae2b8601bcbe20e6f2863b57eb63
Julio Camarero <julio.camarero@liferay.com>
2015-04-14 09:28:14 +0200

    Sample API Client - Create a legacy portlet

---
+++ sample-client-portlet/docroot/WEB-INF/src/com/liferay/content/targeting/sample/SampleClientPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.content.targeting.sample;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Sample portlet using the Content Targeting API
+ *
+ * @author Julio Camarero
+ */
+public class SampleClientPortlet extends MVCPortlet {
+}
