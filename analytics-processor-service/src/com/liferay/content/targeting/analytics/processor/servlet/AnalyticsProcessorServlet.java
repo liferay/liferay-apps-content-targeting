905931405e9c24dd97737ab10a2854b4564c7e57
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-09-08 18:25:03 +0200

    WCM-422 Optimize user and anonymous user detection

--- analytics-processor-service/src/com/liferay/content/targeting/analytics/processor/servlet/AnalyticsProcessorServlet.java
+++ analytics-processor-service/src/com/liferay/content/targeting/analytics/processor/servlet/AnalyticsProcessorServlet.java
@@ -15,6 +15,7 @@
 package com.liferay.content.targeting.analytics.processor.servlet;
 
 import com.liferay.content.targeting.anonymous.users.model.AnonymousUser;
+import com.liferay.content.targeting.anonymous.users.service.AnonymousUserLocalService;
 import com.liferay.content.targeting.anonymous.users.util.AnonymousUsersManager;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -73,6 +74,13 @@ public class AnalyticsProcessorServlet extends HttpServlet {
 				HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e, request,
 				response);
 		}
+	}
+
+	@Reference
+	public void setAnonymousUserLocalService(
+		AnonymousUserLocalService anonymousUserLocalService) {
+
+		_anonymousUserLocalService = anonymousUserLocalService;
 	}
 
 	@Reference
@@ -159,6 +167,16 @@ public class AnalyticsProcessorServlet extends HttpServlet {
 		}
 
 		long anonymousUserId = contextJSONObject.getLong("anonymousUserId", 0);
+		long userId = contextJSONObject.getLong("userId", 0);
+
+		if ((anonymousUserId == 0) && (userId > 0)) {
+			AnonymousUser anonymousUser =
+				_anonymousUserLocalService.fetchAnonymousUserByUserId(userId);
+
+			if (anonymousUser != null) {
+				anonymousUserId = anonymousUser.getAnonymousUserId();
+			}
+		}
 
 		if (anonymousUserId == 0) {
 			AnonymousUser anonymousUser =
@@ -184,6 +202,7 @@ public class AnalyticsProcessorServlet extends HttpServlet {
 			message.put("companyId", companyId);
 			message.put("clientIP", request.getRemoteAddr());
 			message.put("userAgent", request.getHeader(HttpHeaders.USER_AGENT));
+			message.put("userId", userId);
 
 			MessageBusUtil.sendMessage("liferay/analytics", message);
 		}
@@ -201,6 +220,7 @@ public class AnalyticsProcessorServlet extends HttpServlet {
 	private static Log _log = LogFactoryUtil.getLog(
 		AnalyticsProcessorServlet.class);
 
+	private AnonymousUserLocalService _anonymousUserLocalService;
 	private AnonymousUsersManager _anonymousUsersManager;
 
 }
