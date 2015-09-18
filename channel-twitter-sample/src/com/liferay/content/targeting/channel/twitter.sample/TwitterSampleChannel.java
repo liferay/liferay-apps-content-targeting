34d628ca3f007f033afef17bf680c6d437c89879
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-09-18 15:23:57 +0200

    DEVCON2015 Twitter Sample Channel - Add

---
+++ channel-twitter-sample/src/com/liferay/content/targeting/channel/twitter.sample/TwitterSampleChannel.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.channel.twitter.sample;
+
+import com.liferay.content.targeting.InvalidChannelException;
+import com.liferay.content.targeting.api.model.BaseChannel;
+import com.liferay.content.targeting.api.model.Channel;
+import com.liferay.content.targeting.model.ChannelInstance;
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.util.Locale;
+import java.util.Map;
+
+import com.liferay.portal.kernel.util.StringPool;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+/**
+ * @author Brian Chan
+ */
+@Component(immediate = true, service = Channel.class)
+public class TwitterSampleChannel extends BaseChannel {
+
+	@Activate
+	@Override
+	public void activate() {
+		super.activate();
+	}
+
+	@Deactivate
+	@Override
+	public void deActivate() {
+		super.deActivate();
+	}
+
+	@Override
+	public String getIcon() {
+		return "icon-twitter";
+	}
+
+	@Override
+	public String getSummary(ChannelInstance channelInstance, Locale locale) {
+		return LanguageUtil.get(locale, channelInstance.getTypeSettings());
+	}
+
+	@Override
+	public String processChannel(
+		PortletRequest request, PortletResponse response, String id,
+		Map<String, String> values) throws InvalidChannelException {
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	protected void populateContext(
+		ChannelInstance channelInstance, Map<String, Object> context,
+		Map<String, String> values) {
+	}
+
+}
