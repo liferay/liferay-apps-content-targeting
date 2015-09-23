e825fcfe7e7b30944f2a973890cd0990f7abe260
Eduardo Garcia <eduardo.garcia@liferay.com>
2015-09-23 17:50:55 +0200

    DEVCON2015 Twitter Sample Report - Add

---
+++ report-twitter-sample/src/com/liferay/content/targeting/report/twitter.sample/TwitterSampleReport.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.report.twitter.sample;
+
+import com.liferay.content.targeting.api.model.BaseReport;
+import com.liferay.content.targeting.api.model.Report;
+import com.liferay.content.targeting.model.Campaign;
+import com.liferay.content.targeting.model.ReportInstance;
+import com.liferay.portal.kernel.util.StringPool;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Eduardo Garcia
+ */
+@Component(
+	immediate = true, service = Report.class)
+public class TwitterSampleReport extends BaseReport {
+
+	@Activate
+	@Override
+	public void activate() {
+		super.activate();
+	}
+
+	@Deactivate
+	@Override
+	public void deActivate() {
+		super.deActivate();
+	}
+
+	@Override
+	public boolean isInstantiable() {
+		return true;
+	}
+
+	@Override
+	public String getIcon() {
+		return "icon-puzzle";
+	}
+
+	@Override
+	public String getReportType() {
+		return Campaign.class.getName();
+	}
+
+	@Override
+	public void updateReport(ReportInstance reportInstance) {
+	}
+
+	@Override
+	protected void populateContext(
+		ReportInstance reportInstance, Map<String, Object> context) {
+	}
+
+}
