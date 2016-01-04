7a58cd71611ded4e2b28ec8838d38a80720c5100
Pavel Savinov <pavel.savinov@liferay.com>
2016-01-04 13:16:20 +0100

    CT-Analytics-WEB dynamic includes to use analytics tracking js files

---
+++ analytics-web/src/com/liferay/content/targeting/analytics/web/servlet/taglib/AnalyticsBottomDynamicInclude.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.analytics.web.servlet.taglib;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
+import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(immediate = true, service = DynamicInclude.class)
+public class AnalyticsBottomDynamicInclude extends BaseDynamicInclude {
+
+    @Override
+    public void include(
+            HttpServletRequest request, HttpServletResponse response,
+            String key)
+        throws IOException {
+
+        RequestDispatcher requestDispatcher =
+            _servletContext.getRequestDispatcher(_JSP_PATH);
+
+        try {
+            requestDispatcher.include(request, response);
+        }
+        catch (ServletException se) {
+            _log.error("Unable to include JSP " + _JSP_PATH, se);
+
+            throw new IOException("Unable to include JSP " + _JSP_PATH, se);
+        }
+
+    }
+
+    @Override
+    public void register(DynamicIncludeRegistry dynamicIncludeRegistry) {
+        dynamicIncludeRegistry.register("/html/common/themes/bottom.jsp#post");
+    }
+
+    @Reference(
+        target = "(osgi.web.symbolicname=com.liferay.content.targeting.analytics.web)",
+        unbind = "-"
+    )
+    protected void setServletContext(ServletContext servletContext) {
+        _servletContext = servletContext;
+    }
+
+    private static final String _JSP_PATH =
+        "/html/common/themes/bottom-ext.jsp";
+
+    private static final Log _log = LogFactoryUtil.getLog(
+        AnalyticsBottomDynamicInclude.class);
+
+    private volatile ServletContext _servletContext;
+
+}
