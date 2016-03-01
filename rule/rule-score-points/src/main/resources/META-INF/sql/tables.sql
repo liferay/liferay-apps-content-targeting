404633e9348547e743af1c091129dfdc33d8d69d
Eduardo Garcia <eduardo.garcia@liferay.com>
2016-03-01 19:13:18 +0100

    WCM-548 Remove old files

--- rule/rule-score-points/src/main/resources/META-INF/sql/tables.sql
+++
@@ -1,8 +0,0 @@
-create table CT_ScorePoints_ScorePoint (
-	uuid_ VARCHAR(75) null,
-	scorePointId LONG not null primary key,
-	companyId LONG,
-	anonymousUserId LONG,
-	userSegmentId LONG,
-	points LONG
-);
