609ab5c5f7f315fe60ee6a6bca6852db06df4401
Pavel Savinov <pavel.savinov@liferay.com>
2016-03-01 08:29:57 +0100

    WCM-546 separate anonymous users api and service bundles

--- anonymous-users/anonymous-users-service/src/main/java/com/liferay/content/targeting/anonymous/users/model/impl/AnonymousUserModelImpl.java
+++ anonymous-users/anonymous-users-service/src/main/java/com/liferay/content/targeting/anonymous/users/model/impl/AnonymousUserModelImpl.java
@@ -104,13 +104,13 @@ public class AnonymousUserModelImpl extends BaseModelImpl<AnonymousUser>
 	public static final String DATA_SOURCE = "liferayDataSource";
 	public static final String SESSION_FACTORY = "liferaySessionFactory";
 	public static final String TX_MANAGER = "liferayTransactionManager";
-	public static final boolean ENTITY_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.content.targeting.anonymous.users.api.util.ServiceProps.get(
+	public static final boolean ENTITY_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.content.targeting.anonymous.users.service.util.ServiceProps.get(
 				"value.object.entity.cache.enabled.com.liferay.content.targeting.anonymous.users.model.AnonymousUser"),
 			true);
-	public static final boolean FINDER_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.content.targeting.anonymous.users.api.util.ServiceProps.get(
+	public static final boolean FINDER_CACHE_ENABLED = GetterUtil.getBoolean(com.liferay.content.targeting.anonymous.users.service.util.ServiceProps.get(
 				"value.object.finder.cache.enabled.com.liferay.content.targeting.anonymous.users.model.AnonymousUser"),
 			true);
-	public static final boolean COLUMN_BITMASK_ENABLED = GetterUtil.getBoolean(com.liferay.content.targeting.anonymous.users.api.util.ServiceProps.get(
+	public static final boolean COLUMN_BITMASK_ENABLED = GetterUtil.getBoolean(com.liferay.content.targeting.anonymous.users.service.util.ServiceProps.get(
 				"value.object.column.bitmask.enabled.com.liferay.content.targeting.anonymous.users.model.AnonymousUser"),
 			true);
 	public static final long COMPANYID_COLUMN_BITMASK = 1L;
@@ -165,7 +165,7 @@ public class AnonymousUserModelImpl extends BaseModelImpl<AnonymousUser>
 		return models;
 	}
 
-	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(com.liferay.content.targeting.anonymous.users.api.util.ServiceProps.get(
+	public static final long LOCK_EXPIRATION_TIME = GetterUtil.getLong(com.liferay.content.targeting.anonymous.users.service.util.ServiceProps.get(
 				"lock.expiration.time.com.liferay.content.targeting.anonymous.users.model.AnonymousUser"));
 
 	public AnonymousUserModelImpl() {
