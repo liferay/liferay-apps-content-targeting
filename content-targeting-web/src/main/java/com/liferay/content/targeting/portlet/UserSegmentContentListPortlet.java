a89a5a6d2f9e7100d8e1dc865caaa27b8b220131
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-03-02 17:09:03 +0100

    WCM-539 User Segment Content List - Removes custom css

--- content-targeting-web/src/main/java/com/liferay/content/targeting/portlet/UserSegmentContentListPortlet.java
+++ content-targeting-web/src/main/java/com/liferay/content/targeting/portlet/UserSegmentContentListPortlet.java
@@ -60,7 +60,6 @@ import org.osgi.service.component.annotations.Reference;
 	property = {
 		"com.liferay.portlet.css-class-wrapper=user-segment-content-list-portlet",
 		"com.liferay.portlet.display-category=category.ct",
-		"com.liferay.portlet.header-portlet-css=/css/user_segment_content_list/main.css",
 		"com.liferay.portlet.header-portlet-javascript=/js/content_targeting/thumbnails_preview.js",
 		"com.liferay.portlet.icon=/icons/user_segment_content_list.png",
 		"com.liferay.portlet.instanceable=true",
