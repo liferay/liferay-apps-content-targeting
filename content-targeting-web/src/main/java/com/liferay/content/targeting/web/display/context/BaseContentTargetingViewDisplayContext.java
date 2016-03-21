27d911c146f8a7ccb6ed7044b2810c905bb8a60f
Juergen Kappler <jurgen.kappler@liferay.com>
2016-03-21 11:21:47 +0100

    WCM-558 Create base class for view display context

---
+++ content-targeting-web/src/main/java/com/liferay/content/targeting/web/display/context/BaseContentTargetingViewDisplayContext.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.web.display.context;
+
+import com.liferay.content.targeting.util.PortletKeys;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class BaseContentTargetingViewDisplayContext {
+
+	public BaseContentTargetingViewDisplayContext(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		this.liferayPortletRequest = liferayPortletRequest;
+		this.liferayPortletResponse = liferayPortletResponse;
+
+		this.request = PortalUtil.getHttpServletRequest(
+			this.liferayPortletRequest);
+	}
+
+	public String getDisplayStyle() {
+		if (Validator.isNotNull(_displayStyle)) {
+			return _displayStyle;
+		}
+
+		String displayStyle = ParamUtil.getString(request, "displayStyle");
+
+		PortalPreferences portalPreferences =
+			PortletPreferencesFactoryUtil.getPortalPreferences(request);
+
+		if (Validator.isNull(displayStyle)) {
+			displayStyle = portalPreferences.getValue(
+				PortletKeys.CT_ADMIN, "display-style", "list");
+		}
+		else {
+			portalPreferences.setValue(
+				PortletKeys.CT_ADMIN, "display-style", displayStyle);
+		}
+
+		_displayStyle = displayStyle;
+
+		return _displayStyle;
+	}
+
+	public String getKeywords() {
+		if (Validator.isNotNull(_keywords)) {
+			return _keywords;
+		}
+
+		_keywords = ParamUtil.getString(request, "keywords");
+
+		return _keywords;
+	}
+
+	public String getOrderByCol() {
+		if (Validator.isNotNull(_orderByCol)) {
+			return _orderByCol;
+		}
+
+		_orderByCol = ParamUtil.getString(
+			request, "orderByCol", "modified-date");
+
+		return _orderByCol;
+	}
+
+	public String getOrderByType() {
+		if (Validator.isNotNull(_orderByType)) {
+			return _orderByType;
+		}
+
+		_orderByType = ParamUtil.getString(request, "orderByType", "asc");
+
+		return _orderByType;
+	}
+
+	public boolean isDescriptiveView() {
+		if (getDisplayStyle().equals("descriptive")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isIconView() {
+		if (getDisplayStyle().equals("icon")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isListView() {
+		if (getDisplayStyle().equals("list")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	protected final LiferayPortletRequest liferayPortletRequest;
+	protected final LiferayPortletResponse liferayPortletResponse;
+	protected final HttpServletRequest request;
+
+	private String _displayStyle;
+	private String _keywords;
+	private String _orderByCol;
+	private String _orderByType;
+
+}
