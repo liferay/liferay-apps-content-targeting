c798370c0bc4aca23da2d2749ae58210c79a3720
Juergen Kappler <jkappler@Juergens-MBP.liferay.org.es>
2016-03-28 10:26:20 +0200

    WCM-558 New action command to change display views

---
+++ content-targeting-web/src/main/java/com/liferay/content/targeting/web/portlet/action/UpdateDisplayViewMVCActionCommand.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.web.portlet.action;
+
+import com.liferay.content.targeting.util.PortletKeys;
+import com.liferay.content.targeting.web.portlet.ContentTargetingMVCCommand;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + PortletKeys.CT_ADMIN,
+		"mvc.command.name=" + ContentTargetingMVCCommand.UPDATE_DISPLAY_VIEW
+	},
+	service = MVCActionCommand.class
+)
+public class UpdateDisplayViewMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest request, ActionResponse response)
+		throws Exception {
+
+		hideDefaultSuccessMessage(request);
+
+		String displayStyle = ParamUtil.getString(request, "displayStyle");
+
+		PortalPreferences portalPreferences =
+			PortletPreferencesFactoryUtil.getPortalPreferences(request);
+
+		portalPreferences.setValue(
+			PortletKeys.CT_ADMIN, "display-style", displayStyle);
+	}
+
+}
