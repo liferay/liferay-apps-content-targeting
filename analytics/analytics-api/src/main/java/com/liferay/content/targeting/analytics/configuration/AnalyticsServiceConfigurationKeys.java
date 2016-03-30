3043369d26bfe324667e3431b86e457fd4e309fd
Eduardo Garcia <eduardo.garcia@liferay.com>
2016-03-30 13:32:16 +0200

    WCM-556 Group all properties

---
+++ analytics/analytics-api/src/main/java/com/liferay/content/targeting/analytics/configuration/AnalyticsServiceConfigurationKeys.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.analytics.configuration;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class AnalyticsServiceConfigurationKeys {
+
+	public static final String ANALYTICS_CONTENT_ENABLED =
+		"content.targeting.analytics.content.enabled";
+
+	public static final String ANALYTICS_EVENTS_CHECK_INTERVAL =
+		"content.targeting.analytics.events.check.interval";
+
+	public static final String ANALYTICS_EVENTS_MAX_AGE =
+		"content.targeting.analytics.events.max.age";
+
+	public static final String ANALYTICS_FLUSH_INTERVAL =
+		"content.targeting.analytics.flush.interval";
+
+	public static final String ANALYTICS_FORM_ENABLED =
+		"content.targeting.analytics.form.enabled";
+
+	public static final String ANALYTICS_FORM_EXCLUDED_IDS_REGEX =
+		"content.targeting.analytics.form.excluded.ids.regex";
+
+	public static final String ANALYTICS_FORM_INTERACT_ENABLED =
+		"content.targeting.analytics.form.interact.enabled";
+
+	public static final String ANALYTICS_FORM_SUBMIT_ENABLED =
+		"content.targeting.analytics.form.submit.enabled";
+
+	public static final String ANALYTICS_FORM_VIEW_ENABLED =
+		"content.targeting.analytics.form.view.enabled";
+
+	public static final String ANALYTICS_LINK_CLICK_ENABLED =
+		"content.targeting.analytics.link.click.enabled";
+
+	public static final String ANALYTICS_LINK_ENABLED =
+		"content.targeting.analytics.link.enabled";
+
+	public static final String ANALYTICS_LINK_EXCLUDED_IDS_REGEX =
+		"content.targeting.analytics.link.excluded.ids.regex";
+
+	public static final String ANALYTICS_PAGE_ENABLED =
+		"content.targeting.analytics.page.enabled";
+
+	public static final String ANALYTICS_YOUTUBE_ENABLED =
+		"content.targeting.analytics.youtube.enabled";
+
+}
