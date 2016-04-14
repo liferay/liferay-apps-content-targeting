1d74ef5c0d414061ce908103bd3384d6602cb04e
Juergen Kappler <jurgen.kappler@liferay.com>
2016-04-14 16:34:27 +0200

    WCM-573 Show icon for user segment editing in summary view

---
+++ content-targeting-web/src/main/java/com/liferay/content/targeting/web/portlet/configuration/icon/EditUserSegmentPortletConfigurationIcon.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.targeting.web.portlet.configuration.icon;
+
+import com.liferay.content.targeting.util.PortletKeys;
+import com.liferay.content.targeting.web.portlet.ContentTargetingMVCCommand;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + PortletKeys.CT_ADMIN,
+		"path=" + ContentTargetingMVCCommand.VIEW_USER_SEGMENT
+	},
+	service = PortletConfigurationIcon.class
+)
+public class EditUserSegmentPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return LanguageUtil.get(
+			getResourceBundle(getLocale(portletRequest)), "edit");
+	}
+
+	@Override
+	public String getMethod() {
+		return "get";
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		long userSegmentId = ParamUtil.getLong(portletRequest, "userSegmentId");
+
+		PortletURL portletURL = PortletURLFactoryUtil.create(
+			themeDisplay.getRequest(), PortalUtil.getPortletId(portletRequest),
+			themeDisplay.getLayout(), PortletRequest.RENDER_PHASE);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName",
+			ContentTargetingMVCCommand.EDIT_USER_SEGMENT);
+		portletURL.setParameter(
+			"backURL", PortalUtil.getCurrentURL(portletRequest));
+		portletURL.setParameter("userSegmentId", String.valueOf(userSegmentId));
+		portletURL.setParameter(
+			"redirect", PortalUtil.getCurrentURL(portletRequest));
+
+		return portletURL.toString();
+	}
+
+	@Override
+	public double getWeight() {
+		return 101;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		String tabs1 = ParamUtil.getString(portletRequest, "tabs1");
+
+		return Validator.equals(tabs1, "summary");
+	}
+
+	public boolean isToolTip() {
+		return false;
+	}
+
+}
